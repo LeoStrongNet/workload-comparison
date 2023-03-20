7178c87aec5a031e5f0d1c9d5340bfc2486e9764
Leo Litrico <leo@strong.network>
2023-03-20 12:56:06 +0000

    finished benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 import os
+import sys
 
 def get_events_per_second(text):
     pattern = r"events per second:\s+([\d.]+)"
@@ -38,18 +39,28 @@ report += "\n"
 
 #ram usage 
 report += "Memory usage:\n"
-result = subprocess.run(["sysbench", "--test=memory", "run"], stdout=subprocess.PIPE)
-value = result.stdout.decode('utf-8')
-report += "\tMiB per seconds: " + get_mib_per_seconds(value) + "\n"
+for i in range(1, 5):
+    gigabytes = i * 100
+    v = str(gigabytes * 1000000000)
+    totalSize = "--memory-total-size=" + v
+    result = subprocess.run(["sysbench", "--test=memory", totalSize, "run"], stdout=subprocess.PIPE)
+    value = result.stdout.decode('utf-8')
+    report += "\ttotal size: " +  str(gigabytes) + "G -> MiB per seconds: " + get_mib_per_seconds(value) + "\n"
 report += "\n"
 
 #io latency
 report += "FileIO usage:\n"
-result = subprocess.run(["sysbench", "--test=fileio", "--file-test-mode=seqwr", "run"], stdout=subprocess.PIPE)
-value = result.stdout.decode('utf-8')
-report += "\tThroughput (MiB/s): " + get_throughput(value) + "\n"
+for i in range(1, 5):
+    v = str(i * 10)
+    totalSize = "--file-total-size=" + v
+    result = subprocess.run(["sysbench", "--test=fileio", "--file-test-mode=seqwr", "run"], stdout=subprocess.PIPE)
+    
+    #remove all the test files
+    os.system("rm test_file.*")
+    value = result.stdout.decode('utf-8')
+    report += "\ttotal size: " + v + "G -> throughput (MiB/s): " + get_throughput(value) + "\n"
 
-#remove all the test files
-os.system("rm test_file.*")
-
+filename = sys.argv[1] + ".txt"
+with open(filename, "w") as file:
+    file.write(report)
 print(report)
